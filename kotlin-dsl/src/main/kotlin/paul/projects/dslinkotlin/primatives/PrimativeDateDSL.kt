9b7d386a0c742501808877031386d9c9534b0c0f
Paul Wood <paulw@labs.epiuse.com>
2020-01-03 14:09:58 +0200

    Some more simple DSL's

---
+++ kotlin-dsl/src/main/kotlin/paul/projects/dslinkotlin/primatives/PrimativeDateDSL.kt
@@ -0,0 +1,18 @@
+package paul.projects.dslinkotlin.primatives
+
+import java.time.LocalDate
+import java.time.Period
+
+fun main() {
+
+	println(1.days.fromNow)
+	print(1.days.ago)
+}
+
+val Int.days : Period get() = Period.ofDays(this)
+
+val Period.ago : LocalDate get() = LocalDate.now() - this
+
+val Period.fromNow : LocalDate get() = LocalDate.now() + this
+
+
