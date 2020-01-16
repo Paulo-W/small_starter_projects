473f062748b8982c72e7d542234f52e6d42f517d
Paul Wood <paulw@labs.epiuse.com>
2020-01-16 21:13:39 +0200

    initial commit

---
+++ kotlin-basics/src/main/kotlin/paul/small/projects/kotlinbasics/KotlinBasicsApplication.kt
@@ -0,0 +1,11 @@
+package paul.small.projects.kotlinbasics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBasicsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBasicsApplication>(*args)
+}
---
+++ kotlin-basics/src/test/kotlin/paul/small/projects/kotlinbasics/KotlinBasicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package paul.small.projects.kotlinbasics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBasicsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
